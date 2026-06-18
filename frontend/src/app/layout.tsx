b9d200fe2374ac91af1808186298632647a9e5a6
mcyiyue <jngwiemalmsteen@gmail.com>
2026-06-18 10:13:04 +0800

    UI: Fix width of submodule wrapper so it doesn't shrink horizontally

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -59,7 +59,7 @@ export default function RootLayout({
 
         {!isLoginPage && <Sidebar />}
         <main className={`flex-1 overflow-y-auto bg-background/50 h-screen transition-colors duration-300 flex flex-col`}>
-          <div className="flex-1 flex flex-col min-h-0">
+          <div className="flex-1 flex flex-col min-h-0 w-full">
             {children}
           </div>
           {!isLoginPage && pathname !== '/chat' && (
