05c2b0107e8276537f7b0abff80292257f0fdad3
unknown <jngwiemalmsteen@gmail.com>
2026-06-19 19:17:10 +0800

    Remove temporary backend test scripts

--- backend/check.ts
+++
@@ -1,6 +0,0 @@
-import { getAllDocuments } from './src/services/chroma.service';
-getAllDocuments().then(data => {
-  const metadatas = data.metadatas || [];
-  const titles = new Set(metadatas.map((m: any) => m.title));
-  console.log(Array.from(titles));
-}).catch(console.error);
